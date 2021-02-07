40c206e954b8fa2657675d7435f7884ba89f0824
Faizan-Mushtaq <faizan.wani234@gmail.com>
2021-02-07 11:18:02 +0530

    Add Code

--- Prog1_BresenhamsLineDraw.cpp
+++ Prog1_BresenhamsLineDraw.cpp
@@ -1,75 +1,72 @@
-#include <iostream>
-#include <GL/glut.h>
-#include <time.h>
+#include<iostream>
+#include<GL/glut.h>
+#include<time.h>
 using namespace std;
-int x1, x2, yc1, y2;
-int flag = 0;
-void draw_pixel(int x, int y)
+int x1,y1,x2,y2;
+int flag=0;
+void draw_pixel(int x,int y)
 {
-	glColor3f(1, 0, 0);
+	glColor3f(1,0,0);
 	glBegin(GL_POINTS);
-	glVertex2i(x, y);
+	glVertex2i(x,y);
 	glEnd();
 	glFlush();
-
 }
 void draw_line()
 {
-	int dx, dy, i, e;
-	int incx, incy, inc1, inc2;
-	int x, y;
-	dx = x2 - x1;
-	dy = y2 - yc1;
-	if (dx < 0)dx = -dx;
-	if (dy < 0)dy = -dy;
-	incx = 1;
-	if (x2 < x1)
-		incx = -1;
-	incy = 1;
-	if (y2 < yc1)
-		incy = -1;
-	x = x1;
-	y = yc1;
-	if (dx > dy)
+	int dx,dy,i,p;
+	int incx,incy,p1,p2;
+	int x,y;
+	dx=x2-x1;
+	dy=y2-y1;
+	if(dx<0) dx=-dx;
+	if(dy<0) dy=-dy;
+	incx=1;
+	if(x2<x1) incx=-1;
+	incy=1;
+	if(y2<y1) incy=-1;
+	x=x1;
+	y=y1;
+	if(dx > dy)
 	{
-		draw_pixel(x, y);
-		e = 2 * dy - dx;
-		inc1 = 2 * (dy - dx);
-		inc2 = 2 * dy;
-		for (i = 0; i < dx; i++)
+		draw_pixel(x,y);
+		p=2*dy-dx;
+		p1=2*(dy-dx);
+		p2=2*dy;
+		for(i=0;i<dx;i++)
 		{
-			if (e > 0)
+			if(p>0)
 			{
-				y += incy;
-				e += inc1;
+				y+=incy;
+				p+=p1;
 			}
 			else
-				e += inc2;
-			x += incx;
-			draw_pixel(x, y);
+			{
+				p+=p2;
+			}
+			x+=incx;
+			draw_pixel(x,y);
 		}
 	}
 	else
 	{
-
-		draw_pixel(x, y);
-		e = 2 * dx - dy;
-		inc1 = 2 * (dx - dy);
-		inc2 = 2 * dx;
-		for (i = 0; i < dy; i++)
+		draw_pixel(x,y);
+		p=2*dx - dy;
+		p1=2*(dx-dy);
+		p2=2*dx;
+		for(i=0;i<dy;i++)
 		{
-			if (e > 0)
+			if(p>0)
 			{
-				x += incx;
-				e += inc1;
-
+				x+=incx;
+				p+=p1;
 			}
 			else
-
-				e += inc2;
-			y += incy;
-
-			draw_pixel(x, y);
+			{
+				p+=p2;
+			}
+			y+=incy;
+			draw_pixel(x,y);
 		}
 	}
 	glFlush();
@@ -77,63 +74,48 @@ void draw_line()
 void myinit()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glClearColor(1, 1, 1, 1);
-	gluOrtho2D(-250, 250, -250, 250);
+	glClearColor(1,1,1,1);
+	gluOrtho2D(-250,250,-250,250);
 }
-void MyMouse(int button, int state, int x, int y)
+void mymouse(int btn,int st,int x,int y)
 {
-	switch (button)
+	switch(btn)
 	{
-	case GLUT_LEFT_BUTTON:
-		if (state == GLUT_DOWN)
-		{
-			if (flag == 0)
-			{
-				printf("Defining x1,y1");
-				x1 = x - 250;
-				yc1 = 250 - y;
-				flag++;
-				cout << x1 << " " << yc1 << " \n";
-			}
-			else
-			{
-				printf("Defining x2,y2");
-				x2 = x - 250;
-				y2 = 250 - y;
-				flag = 0;
-				cout << x2 << " " << y2 << " \n";
-				draw_line();
-
-			}
-
-		}
-		break;
+			case GLUT_LEFT_BUTTON: if(st==GLUT_DOWN)
+															{
+																if (flag==0)
+																{
+																	printf("def x1 y1");
+																	x1=x-250;
+																	y1=250-y;
+																	flag++;
+																	cout<<x1<<" "<< y1<< endl;
+																}
+																else{
+																	printf("def x2 y2");
+																	x2=x-250;
+																	y2=250-y;
+																	flag=0;
+																	cout<<x2<<" "<< y2<< endl;
+																	draw_line();
+																}
+															}
+															break;
 	}
 }
-void display()
-{}
-int main(int ac, char* av[])
+void display() {}
+int main(int argc,char** argv)
 {
-
-	//FOR KEYBOARD
-	cout<<"X1\n";
-	cin>>x1;
-	cout<<"Y1\n";
-	cin>>yc1;
-	cout<<"X2\n";
-	cin>>x2;
-	cout<<"Y2\n";
-	cin>>y2;
-	//END KEYBOARD
-
-	glutInit(&ac, av);
+	// cout<<"Enter x1 y1 x2 y2\n";
+	// cin>>x1>>y1>>x2>>y2;
+	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(500, 500);
-	glutInitWindowPosition(100, 200);
-	glutCreateWindow("LINE");
+	glutInitWindowSize(500,500);
+	glutInitWindowPosition(100,100);
+	glutCreateWindow("Bre Line");
 	myinit();
-	//glutMouseFunc(MyMouse); //INCLUDE TO USE MOUSE, REMOVE WHILE USING KEYBOARD
-	draw_line(); //INCLUDE TO USE KEYBOARD, REMOVE WHILE USING MOUSE
+	glutMouseFunc(mymouse); //INCLUDE TO USE MOUSE, REMOVE WHILE USING KEYBOARD
+	//draw_line(); //INCLUDE TO USE KEYBOARD, REMOVE WHILE USING MOUSE
 	glutDisplayFunc(display);
 	glutMainLoop();
 }
